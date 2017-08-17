123ea039726bd30d92f1815f8e44de9007fe84d2
shortymike123 <shortymike123@hotmail.co.uk>
2017-08-17 01:42:26 +0000

    began work on database implementation

---
+++ schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE if EXISTS games;
+
+CREATE TABLE games (
+  name text,
+  consoles text,
+  category text
+);
--- test.py
+++ test.py
@@ -1,7 +1,21 @@
-from flask import Flask
-import urllib, json
+from flask import Flask, g
+import urllib, json, sqlite3
 app = Flask(__name__)
+db_location = 'var/games.db'
 
+def get_db():
+  db = getattr(g, 'db', None)
+  if db is None:
+    db = sqlite3.connect(db_location)
+    g.db = db
+  return db
+
+def init_db():
+  with app.app_context():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+      db.cursor().executescript(f.read())
+    db.commit()
 
 @app.route("/")
 def hello():
@@ -12,7 +26,13 @@ def hello():
   output = ''
 
   for x in data['data']:
-    output = output + x['names']['international'] + '\n'
+    name = x['names']['international']
+    consoles = x['platforms']
+    categories = []
+    for y in x['categories']['data']:
+      if y['type'] == 'per-game':
+        categories.append(y['name'])
+    output = output + name + str(consoles) + str(categories) + '<br>'
 
   return output
 
